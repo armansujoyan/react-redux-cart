bb0825e89173c02af7ee36ab948306bb5c9032c8
Arman Sujoyan <asujoyan@gmail.com>
2018-12-25 15:17:08 +0400

    Add simple navigation

---
+++ src/components/navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Navigation() {
+  return (
+    <div>
+      <nav>
+          <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/cart">Cart</NavLink></li>
+          </ul>
+      </nav>
+    </div>
+  )
+}
