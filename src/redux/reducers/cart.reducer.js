199df146fb15189fcd20cf96cbf29a76390a086c
Arman Sujoyan <asujoyan@gmail.com>
2018-12-25 16:03:01 +0400

    Create cart reducer

---
+++ src/redux/reducers/cart.reducer.js
@@ -0,0 +1,15 @@
+import { cartConstants } from '../constants'
+
+const cartReducer = (state=[], action) => {
+  const { type, payload } = action;
+  switch(type){
+    case cartConstants.ADD_TO_CART:
+      return [...state, payload];
+    case cartConstants.REMOVE_FROM_CART:
+      return state.filter(item => item.id !== payload.id);
+    default:
+      return state;
+  }
+}
+
+export default cartReducer;
