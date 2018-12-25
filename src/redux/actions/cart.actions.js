e6154e0e80f16f7f7b4be44d10ea41af49b232d2
Arman Sujoyan <asujoyan@gmail.com>
2018-12-25 16:24:18 +0400

    Change cart action type

--- src/redux/actions/cart.actions.js
+++ src/redux/actions/cart.actions.js
@@ -2,4 +2,4 @@ import { cartConstants } from '../constants'
 
 export const addToCart = (item) => ({ type: cartConstants.ADD_TO_CART, payload: item});
 
-export const removeItem = (id) => ({ type: cartConstants.REMOVE_FROM_CART, payload: id});
+export const removeItem = (item) => ({ type: cartConstants.REMOVE_FROM_CART, payload: item});
